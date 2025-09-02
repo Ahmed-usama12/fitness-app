7b22528d70430a3a5095651d7de6746e6f45baad
Ahmed Refai <momo3956248@gmail.com>
2025-09-02 18:37:28 +0300

    feat(forgot-password): implement form handling with validation using react-hook-form and zod; update email input structure

--- src/app/pages/auth/forgot-password/forgot-password.tsx
+++ src/app/pages/auth/forgot-password/forgot-password.tsx
@@ -1,11 +1,39 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  useForgotPasswordSchema,
+  type ForgotPasswordFields,
+} from "@/lib/schema/auth.schema";
 
-import { Label } from "@/components/ui/label";
 import { useId } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function ForgotPassword() {
+  // Id hook
   const id = useId();
+
+  // Schema
+  const schema = useForgotPasswordSchema();
+
+  // Form
+  const form = useForm<ForgotPasswordFields>({
+    defaultValues: {},
+    resolver: zodResolver(schema),
+  });
+
+  // Submit
+  const onSubmit = (values: ForgotPasswordFields) => {
+    console.log(values);
+  };
 
   return (
     <div>
@@ -13,27 +41,51 @@ export default function ForgotPassword() {
         Forgot Password
       </h1>
       <div className="p-10 rounded-[50px] border">
-        <div className="space-y-6 flex flex-col justify-center items-center">
-          <Label
-            className="text-center text-2xl"
-            htmlFor={id}
+        {/* Form */}
+        <Form {...form}>
+          {" "}
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6"
           >
-            Enter Your Email
-          </Label>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="w-full flex flex-col justify-center items-center">
+                  {/* Label */}
+                  <FormLabel
+                    htmlFor={id}
+                    className="text-center text-2xl"
+                  >
+                    Enter Your Email
+                  </FormLabel>
 
-          <Input
-            id={id}
-            type="email"
-            placeholder="email"
-            className="w-ful"
-          />
-          <Button
-            icon={false}
-            className="w-full"
-          >
-            Send OTP
-          </Button>
-        </div>
+                  {/* Input */}
+                  <FormControl>
+                    <Input
+                      id={id}
+                      type="email"
+                      placeholder="email"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  {/* Error message */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Actions */}
+            <Button
+              icon={false}
+              className="w-full"
+            >
+              Send OTP
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
