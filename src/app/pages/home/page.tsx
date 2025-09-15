bf672f019465c9a5bfab6c80249de0541b36b94f
Ahmed-usama12 <104719818+Ahmed-usama12@users.noreply.github.com>
2025-09-15 15:47:57 +0300

    feat/meals and whyus sections

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,11 +1,19 @@
 import About from "../about/page";
 import HeroSection from "./components/hero-section/hero-section";
+import MealsSection from "./components/meals-section/meals-section";
+import WhyUsSection from "./components/whyus-section/why-us-section";
 
 export default function Homepage() {
   return (
     <main>
       <HeroSection />
       <About />
+
+      {/* whyus section */}
+      <WhyUsSection />
+
+      {/* Meals Section */}
+      <MealsSection />
     </main>
   );
 }
