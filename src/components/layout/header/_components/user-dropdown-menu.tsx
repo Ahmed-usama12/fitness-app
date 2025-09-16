cadce1cea5de1d410307c0484121ea72ec5b069e
shahd202419 <shahd2023future@gmail.com>
2025-09-16 13:41:57 +0300

    feat(implement-login-page):add login and signup buttons to navbar

--- src/components/layout/header/_components/user-dropdown-menu.tsx
+++ src/components/layout/header/_components/user-dropdown-menu.tsx
@@ -6,9 +6,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { LogOut, User } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { userMenuItems } from "./navigation-data";
 import { Separator } from "@/components/ui/separator";
 import { ThemeToggle } from "@/components/common/theme-toggel";
@@ -16,130 +16,103 @@ import { useTranslations } from "use-intl";
 import LanguageMenu from "@/components/common/language-menu";
 import { cn } from "@/lib/utils";
 import { useLoginContext } from "@/context/login-context";
+import { useLogout } from "@/hooks/use-logout";
 
-/**
- * Props interface for UserDropdownMenu component
- */
-interface UserDropdownMenuProps {
-  userEmail?: string;
-  userName?: string;
-}
-
-/**
- * User dropdown menu component for authenticated users
- * Provides access to user profile, settings, and theme toggle
- */
 export function UserDropdownMenu() {
 
   //context
-  const { setToken, user, setUser } = useLoginContext();
-
-  //navigate
-  const navigate = useNavigate()
-
-  //handle logout
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setToken('null');
-    localStorage.removeItem('user');
-    setUser(null);
-    setTimeout(() => {
-      navigate('/auth/login');
-    }, 1000)
-  }
+  const { user } = useLoginContext();
 
   //  Translations
   const t = useTranslations();
 
-  return (
-    <DropdownMenu>
-      {/* Trigger button with user icon */}
-      <DropdownMenuTrigger className="hidden md:flex">
-        <div
-          className={cn(buttonVariants({ variant: "default", size: "icon" }), "size-[47px]")}
-        >
-          <User className="h-5 w-5" />
-        </div>
-      </DropdownMenuTrigger>
+  //Logout
+  const { mutate: logout } = useLogout();
 
-      {/* Dropdown content */}
-      <DropdownMenuContent
-        className="w-56"
-        align="end"
-      >
-        {!user ? (
-          <>
-            {/* Not A User */}
+
+  return (
+    <>
+      {!user ? (
+        <div className="flex items-center gap-6">
+          {/*Login*/}
+          <Link to="/auth/login" className="flex">
+            <Button>LOGIN</Button>
+          </Link>
+
+          {/*Register*/}
+          <Link to="/auth/register" className="flex">
+            <Button variant="secondary">SIGNUP</Button>
+          </Link>
+        </div>
+
+      ) : (
+        <DropdownMenu>
+          {/* Trigger button with user icon */}
+          <DropdownMenuTrigger className="hidden md:flex">
+            <div
+              className={cn(buttonVariants({ variant: "default", size: "icon" }), "size-[47px]")}
+            >
+              <User className="h-5 w-5" />
+            </div>
+          </DropdownMenuTrigger>
+
+          {/* Dropdown content */}
+          <DropdownMenuContent
+            className="w-56"
+            align="end"
+          >
+
+            {/* User information section */}
             <DropdownMenuLabel className="font-normal">
-              <p className="text-sm font-medium">{t("welcome")}</p>
-              <p className="text-xs text-muted-foreground">
-                {t("login-or-register")}
-              </p>
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">{user?.firstName}</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  {user?.email}
+                </p>
+              </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
 
-            <DropdownMenuItem asChild>
-              <Link to="/auth/login" className="w-full">
-                {t("login")}
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link to="/auth/register" className="w-full">
-                {t("register")}
-              </Link>
-            </DropdownMenuItem>
-          </>
-        ) : (<>
-          {/* User information section */}
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{user?.firstName}</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {user?.email}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-
-          {/* Navigation menu items */}
-          {userMenuItems.map((item) => {
-            const IconComponent = item.icon;
-            return (
-              <DropdownMenuItem
-                key={item.name}
-                asChild
-              >
-                <Link
-                  to={item.href}
-                  className="flex items-center w-full"
+            {/* Navigation menu items */}
+            {userMenuItems.map((item) => {
+              const IconComponent = item.icon;
+              return (
+                <DropdownMenuItem
+                  key={item.name}
+                  asChild
                 >
-                  <IconComponent className="mr-2 h-4 w-4" />
-                  <span>{t(item.name)}</span>
-                </Link>
-              </DropdownMenuItem>
-            );
-          })}
-        </>)}
+                  <Link
+                    to={item.href}
+                    className="flex items-center w-full"
+                  >
+                    <IconComponent className="mr-2 h-4 w-4" />
+                    <span>{t(item.name)}</span>
+                  </Link>
+                </DropdownMenuItem>
+              );
+            })}
 
-        {/* Theme toggle section */}
-        <Separator className="my-2" />
-        <DropdownMenuItem>
-          <ThemeToggle />
-        </DropdownMenuItem>
+            {/* Theme toggle section */}
+            <Separator className="my-2" />
+            <DropdownMenuItem>
+              <ThemeToggle />
+            </DropdownMenuItem>
 
-        {/* language toggle section */}
-        <Separator className="my-2" />
-        <DropdownMenuItem>
-          <LanguageMenu />
-        </DropdownMenuItem>
+            {/* language toggle section */}
+            <Separator className="my-2" />
+            <DropdownMenuItem>
+              <LanguageMenu />
+            </DropdownMenuItem>
 
-        {/*logout*/}
-        <Separator className="my-2" />
-        <DropdownMenuItem onClick={handleLogout} className="cursor-pointer ">
-          <LogOut className="me-2 size-4" />
-          <span>{t('logout')}</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            {/*logout*/}
+            <Separator className="my-2" />
+            <DropdownMenuItem onClick={() => logout()} className="cursor-pointer ">
+              <LogOut className="me-2 size-4" />
+              <span>{t('logout')}</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>)}
+    </>
+
   );
 }
