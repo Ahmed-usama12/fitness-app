fffec133e64b3482f35d51b3e8ec10e18facdfd9
Ahmed Refai <momo3956248@gmail.com>
2025-09-27 07:03:26 +0300

    Add getSpecificMeals function to fetch meals by category

--- src/lib/api/meals.api.ts
+++ src/lib/api/meals.api.ts
@@ -17,6 +17,23 @@ export const getMealsCategories = async () => {
     throw error;
   }
 };
+// Get Meals Categories
+export const getSpecificMeals = async (category: string) => {
+  try {
+    const { data }: { data: APIResponse<MealsCategoriesResponse> } = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`,
+    );
+
+    if ("error" in data) throw new Error(data.error);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.log(error);
+    }
+    throw error;
+  }
+};
 
 // Get Meals Categories
 export const getMealsDetails = async (Id: string) => {
